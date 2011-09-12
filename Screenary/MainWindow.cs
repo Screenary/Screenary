bb1d2ec0472d5b4edd5d02fe430b4de87faa672e
Marc-André Moreau <marcandre.moreau@gmail.com>
2011-09-12 19:40:54 -0400

    screenary: initial commit

---
+++ Screenary/MainWindow.cs
@@ -0,0 +1,46 @@
+/**
+ * Screenary: Real-Time Collaboration Redefined.
+ * Main Window
+ *
+ * Copyright 2011 Marc-Andre Moreau <marcandre.moreau@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnQuitActionActivated (object sender, System.EventArgs e)
+	{
+		Application.Quit ();
+	}
+
+	protected void OnAboutActionActivated (object sender, System.EventArgs e)
+	{
+		AboutDialog dialog = new AboutDialog();
+		dialog.Run ();
+	}
+}

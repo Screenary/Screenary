292e7ceb273f3c6e6590ded3c7ca6e2497eb1e03
Marc-André Moreau <marcandre.moreau@gmail.com>
2011-11-28 21:08:23 -0500

    server: make replay mode a command-line option

--- Server/Main.cs
+++ Server/Main.cs
@@ -8,6 +8,8 @@ namespace Screenary.Server
 {
 	class MainClass
 	{
+		public static string replayFile;
+		public static bool replayMode = false;
 		public static string WorkingDirectory;
 		
 		public static void Main(string[] args)
@@ -17,15 +19,24 @@ namespace Screenary.Server
 			if (!WorkingDirectory.EndsWith("/"))
 				WorkingDirectory += "/";
 			
-			Directory.SetCurrentDirectory(WorkingDirectory + "../../");
+			if (args[0].Equals("--replay") && (args.Length > 1))
+			{
+				replayFile = args[1].ToString();
+				Console.WriteLine("Replay Mode, using {0}", replayFile);
+				replayMode = true;
+			}
 			
 			BroadcasterServer server = new BroadcasterServer("127.0.0.1", 4489);
-			PcapReader pcap = new PcapReader(File.OpenRead("data/ferrari.pcap"));
 			
-			foreach (PcapRecord record in pcap)
+			if (replayMode)
 			{
-				PDU pdu = new PDU(record.Buffer, 0, 1);
-				server.addPDU(pdu);
+				PcapReader pcap = new PcapReader(File.OpenRead(replayFile));
+				
+				foreach (PcapRecord record in pcap)
+				{
+					PDU pdu = new PDU(record.Buffer, 0, 1);
+					server.addPDU(pdu);
+				}
 			}
 			
 			while (true)

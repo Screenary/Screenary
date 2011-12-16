6e6fba6b91d8403eb533ed329d2325ad703d7933
Marc-André Moreau <marcandre.moreau@gmail.com>
2011-12-16 16:21:06 -0500

    FreeRDP: added new callbacks for pointer updates

--- FreeRDP/Core/Update/PointerUpdate.cs
+++ FreeRDP/Core/Update/PointerUpdate.cs
@@ -3,11 +3,51 @@ using System.Runtime.InteropServices;
 
 namespace FreeRDP
 {
-	public unsafe delegate void pPointerPosition(rdpContext* context, IntPtr pointerPosition);
-	public unsafe delegate void pPointerSystem(rdpContext* context, IntPtr pointerSystem);
-	public unsafe delegate void pPointerColor(rdpContext* context, IntPtr pointerColor);
-	public unsafe delegate void pPointerNew(rdpContext* context, IntPtr pointerNew);
-	public unsafe delegate void pPointerCached(rdpContext* context, IntPtr pointerCached);
+	[StructLayout(LayoutKind.Sequential)]
+	public unsafe struct PointerPositionUpdate
+	{
+		public UInt32 xPos;
+		public UInt32 yPos;
+	}
+	
+	[StructLayout(LayoutKind.Sequential)]
+	public unsafe struct PointerSystemUpdate
+	{
+		public UInt32 type;
+	}
+	
+	[StructLayout(LayoutKind.Sequential)]
+	public unsafe struct PointerColorUpdate
+	{
+		public UInt32 cacheIndex;
+		public UInt32 xPos;
+		public UInt32 yPos;
+		public UInt32 width;
+		public UInt32 height;
+		public UInt32 lengthAndMask;
+		public UInt32 lengthXorMask;
+		public byte* xorMaskData;
+		public byte* andMaskData;
+	}
+	
+	[StructLayout(LayoutKind.Sequential)]
+	public unsafe struct PointerNewUpdate
+	{
+		public UInt32 xorBpp;
+		public PointerColorUpdate colorPtrAttr;
+	}
+	
+	[StructLayout(LayoutKind.Sequential)]
+	public unsafe struct PointerCachedUpdate
+	{
+		public UInt32 cacheIndex;
+	}
+	
+	public unsafe delegate void pPointerPosition(rdpContext* context, PointerPositionUpdate* pointerPosition);
+	public unsafe delegate void pPointerSystem(rdpContext* context, PointerSystemUpdate* pointerSystem);
+	public unsafe delegate void pPointerColor(rdpContext* context, PointerColorUpdate* pointerColor);
+	public unsafe delegate void pPointerNew(rdpContext* context, PointerNewUpdate* pointerNew);
+	public unsafe delegate void pPointerCached(rdpContext* context, PointerCachedUpdate* pointerCached);
 	
 	[StructLayout(LayoutKind.Sequential)]
 	public unsafe struct rdpPointerUpdate

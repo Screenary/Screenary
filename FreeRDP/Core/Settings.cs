a2d225aa97f0f53dfb3ae720516452fa114c69d2
Marc-André Moreau <marcandre.moreau@gmail.com>
2012-01-21 15:44:05 -0500

    RDP: complete settings mapping

--- FreeRDP/Core/Settings.cs
+++ FreeRDP/Core/Settings.cs
@@ -9,6 +9,7 @@ namespace FreeRDP
 		public freerdp* instance;
 		public fixed UInt32 paddingA[16-1];
 		
+		/* Core Protocol Parameters */
 		public UInt32 width;
 		public UInt32 height;
 		public UInt32 rdpVersion;
@@ -25,6 +26,7 @@ namespace FreeRDP
 		public int authentication;
 		public fixed UInt32 paddingB[48-30];
 		
+		/* Connection Settings */
 		public UInt32 port;
 		public int ipv6;
 		public IntPtr hostname;
@@ -40,8 +42,19 @@ namespace FreeRDP
 		public UInt32 performanceFlags;
 		public fixed UInt32 paddingC[80-61];
 		
-		public fixed UInt32 paddingD[112-80];
+		/* User Interface Parameters */
+		public int softwareGdi;
+		public int workarea;
+		public int fullscreen;
+		public int grabKeyboard;
+		public int decorations;
+		public UInt32 percentScreen;
+		public int mouseMotion;
+		public IntPtr windowTitle;
+		public UInt64 parentWindowXid;
+		public fixed UInt32 paddingD[112-89];
 		
+		/* Internal Parameters */
 		public IntPtr homePath;
 		public UInt32 shareId;
 		public UInt32 pduSource;
@@ -49,11 +62,130 @@ namespace FreeRDP
 		public int serverMode;
 		public fixed UInt32 paddingE[144-117];
 		
+		/* Security */
 		public int encryption;
 		public int tlsSecurity;
 		public int nlaSecurity;
 		public int rdpSecurity;
 		public fixed UInt32 paddingF[160-148];
+		
+		/* Session */
+		public int consoleAudio;
+		public int consoleSession;
+		public UInt32 redirectedSessionId;
+		public fixed UInt32 paddingG[176-163];
+	
+		/* Output Control */
+		public int refreshRect;
+		public int suppressOutput;
+		public int desktopResize;
+		public fixed UInt32 paddingH[192-179];
+	
+		/* Reconnection */
+		public int autoReconnection;
+		public IntPtr clientAutoReconnectCookie;
+		public IntPtr serverAutoReconnectCookie;
+		public fixed UInt32 paddingI[208-195];
+	
+		/* Time Zone */
+		public IntPtr clientTimeZone;
+		public fixed UInt32 paddingJ[216-209];
+	
+		/* Capabilities */
+		public UInt32 osMajorType;
+		public UInt32 osMinorType;
+		public UInt32 vcChunkSize;
+		public int soundBeeps;
+		public int smoothFonts;
+		public int frameMarker;
+		public int fastpathInput;
+		public int fastpathOutput;
+		public fixed byte receivedCaps[32];
+		public fixed byte orderSupport[32];
+		public int surfaceCommands;
+		public int disableWallpaper;
+		public int disableFullWindowDrag;
+		public int disableMenuAnimations;
+		public int disableTheming;
+		public UInt32 connectionType;
+		public UInt32 multifragMaxRequestSize;
+		public fixed UInt32 paddingK[248-247];
+	
+		/* Certificate */
+		public IntPtr certFile;
+		public IntPtr privateKeyFile;
+		public fixed char clientHostname[32];
+		public fixed char clientProductId[32];
+		public IntPtr serverRandom;
+		public IntPtr serverCertificate;
+		public int ignoreCertificate;
+		public IntPtr serverCert;
+		public fixed UInt32 paddingL[280-270];
+	
+		/* Codecs */
+		public int rfxCodec;
+		public int nsCodec;
+		public UInt32 rfxCodecId;
+		public UInt32 nsCodecId;
+		public UInt32 rfxCodecMode;
+		public int frameAcknowledge;
+		public fixed UInt32 paddingM[296-286];
+	
+		/* Recording */
+		public int dumpRfx;
+		public int playRfx;
+		public char* dumpRfxFile;
+		public char* playRfxFile;
+		public fixed UInt32 paddingN[312-300];
+	
+		/* RemoteApp */
+		public int remoteApp;
+		public UInt32 numIconCaches;
+		public UInt32 numIconCacheEntries;
+		public int railLangbarSupported;
+		public fixed UInt32 paddingO[320-316];
+	
+		/* Pointer */
+		public int largePointer;
+		public int colorPointer;
+		public UInt32 pointerCacheSize;
+		public fixed UInt32 paddingP[328-323];
+	
+		/* Bitmap Cache */
+		public int bitmapCache;
+		public int bitmapCacheV3;
+		public int persistentBitmapCache;
+		public UInt32 bitmapCacheV2NumCells;
+		public IntPtr bitmapCacheV2CellInfo;
+		public fixed UInt32 paddingQ[344-333];
+	
+		/* Offscreen Bitmap Cache */
+		public int offscreenBitmapCache;
+		public UInt32 offscreenBitmapCacheSize;
+		public UInt32 offscreenBitmapCacheEntries;
+		public fixed UInt32 paddingR[352-347];
+	
+		/* Glyph Cache */
+		public int glyph_cache;
+		public UInt32 glyphSupportLevel;
+		public IntPtr glyphCache;
+		public IntPtr fragCache;
+		public fixed UInt32 paddingS[360-356];
+	
+		/* Draw Nine Grid */
+		public int drawNineGrid;
+		public UInt32 drawNineGridCacheSize;
+		public UInt32 drawNineGridCacheEntries;
+		public fixed UInt32 paddingT[368-363];
+	
+		/* Draw GDI+ */
+		public int drawGdiPlus;
+		public int drawGdiPlusCache;
+		public fixed UInt32 paddingU[376-370];
+	
+		/* Desktop Composition */
+		public int desktopComposition;
+		public fixed UInt32 paddingV[384-377];
 	};
 }
 
